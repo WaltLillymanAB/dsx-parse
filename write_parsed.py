89db2da91bce018633c2881430c5de8305295320
WaltLillymanAB <walter.lillyman@anheuser-busch.com>
2023-09-13 20:26:33 -0500

    Removed s-string code, not needed here.

--- write_parsed.py
+++ write_parsed.py
@@ -10,44 +10,40 @@ project_file='EDW_DW1_PROD'
 pickle_file=project_file + '.pkl'
 pretty_file=project_file + '_pp.txt'
 parsed_file=project_file + '_parsed.txt'
-interest_items='interest_items.txt'
+interest_items='interest_items'
 
 # Load the dictionary from a .pkl file created by dsx-parse.py
 print(f'{strftime("%Y-%m-%d %H:%M:%S")}  #{getframeinfo(currentframe()).lineno}: Loading pickle file into a dictionary.')
 with open(pickle_file, 'rb') as f:
   d = pickle.load(f)
 
-s=''           # Initialize tring to accumulate result.
-fd='¥'         # Field delimiter in output, Alt-0165
 crlf='[\r\n]'  # Carriage-return and line-feed pattern to replace.
 nl=' '         # Replacement text for embedded CR+LF.
 
+# StageTypes of interest:
+stage_types_desired=['CODBCSTAGE','ODBCCONNECTOR','ODBCCONNECTORPX','ORACLECONNECTOR','ORACLECONNECTORPX','ORAOCI9','PXODBC','PXORACLE']
+
 # Counters:
-j_cnt=0
-r_cnt=0
-s_cnt=0
+j_cnt=0; r_cnt=0; s_cnt=0
 
 # Keep track of current position:
-job=''
-record=''
-subrecord=''
+job=''; record=''; subrecord=''
+
+file_name = strftime("%Y%m%d_%H%M%S")+'_'+parsed_file
 
 # Capture items of interest to a file:
-with open(interest_items, mode='w', encoding="utf-8") as f:
-  # Print output file header record:
-  f.write('DSX Project\tJob #\tJob name\tRecord #\tOLEType\tRecord Name\tRecord Category\tSubrecord #\tSubrecord Name\tSubrecord Value\n')
+with open(interest_items+'_'+strftime("%Y%m%d_%H%M%S")+'.txt', mode='w', encoding="utf-8") as f:
+  # Print header record:
+  f.write('DSX Project\tJob #\tJob name\tRecord #\tOLEType\tRecord Name\tRecord Category\tInputPins\tStageType\tSubrecord #\tSubrecord Name\tSubrecord Value\n')
 
   # Reset vars that will save contents of items of interest for writing each line to a file per record/subrecord.:
-  identifier=''; rec_oletype=''; rec_name=''; rec_category=''; subrec_name=''; subrec_value=''; is_usersql=False; wrote_record=False
+  identifier=''; rec_oletype=''; rec_name=''; rec_category=''; rec_inputpins=''; rec_stagetype=''; subrec_name=''; subrec_value=''; is_usersql=False; wrote_record=False
 
   print(f'{strftime("%Y-%m-%d %H:%M:%S")}  #{getframeinfo(currentframe()).lineno}: Parsing dictionary contents.')
-  # There's one header per DSX project file. Get some "header" attributes:
+  # There's one header per DSX project file. Get the header attribute for this DSX project name:
   i = d.properties.get('header')
-  # for j in ['servername','toolinstanceid']:
   for j in ['toolinstanceid']:
-    s += j + fd  # The key
     t = re.sub(crlf, nl, i.get(j))  # Replace cr+lf
-    s += t + fd  # The value
     project = t
 
   # There's many "job" records per project file. Get some "job" attributes:
@@ -55,11 +51,8 @@ with open(interest_items, mode='w', encoding="utf-8") as f:
     j_cnt += 1
     print(f'{strftime("%Y-%m-%d %H:%M:%S")}  #{getframeinfo(currentframe()).lineno}: Job {j_cnt}...')
     job = str(j_cnt)
-    s += f'Job' + fd + job + fd
     for j in ['identifier']: 
-      s += j + fd  # The key
-      t = re.sub(crlf, nl, i[j])  # Replace cr+lf
-      s += t + fd  # The value
+      t = re.sub(crlf, nl, i[j]) 
       identifier = t
 
     # There's many "records" per job. Get some "records" attributes:
@@ -67,25 +60,25 @@ with open(interest_items, mode='w', encoding="utf-8") as f:
       r_cnt += 1
       record = r_cnt
       # print(f'{strftime("%Y-%m-%d %H:%M:%S")}  #{getframeinfo(currentframe()).lineno}: Job {j_cnt}, record {r_cnt}...')
-      s += f'Record' + fd + str(r_cnt) + fd 
-      for k in ['oletype','name','category']:
+      for k in ['oletype','name','category','inputpins','stagetype']:
         if j.get(k) is not None:  # j.get('oletype')
-          s += k + fd
           t = re.sub(crlf, nl, j.get(k))
-          s += t + fd
           if k=='oletype': rec_oletype=t
           if k=='name': rec_name=t
           if k=='category': rec_category=t
+          if k=='inputpins': rec_inputpins=t
+          if k=='stagetype': rec_stagetype=t
+      # Keep inputpins & stagetype only for desired stagetypes:
+      if rec_stagetype not in stage_types_desired:
+        rec_inputpins=''
+        rec_stagetype=''
       
       # There's many "subrecords" per record.
       for m in j.get('subrecords'):  # For each dict in the list.
         s_cnt += 1
         subrecord = s_cnt
-        s += f'Subrecord' + fd + str(s_cnt) + fd
         for p in m:  # For each key in the dict.
-          s += p + fd
           t = re.sub(crlf, nl, m.get(p))
-          s += t + fd
           if p=='tabledef': 
             subrec_name=p
             subrec_value=t
@@ -100,28 +93,19 @@ with open(interest_items, mode='w', encoding="utf-8") as f:
                 is_usersql=False
         # At the end of each subrecord, if subrec_name was populated, print the row to the file:
         if subrec_name != '':
-          f.write(project+'\t'+str(job)+'\t'+identifier+'\t'+str(record)+'\t'+rec_oletype+'\t'+rec_name+'\t'+rec_category+'\t'+str(subrecord)+'\t'+subrec_name+'\t'+subrec_value+'\n')  # Header record
+          f.write(project+'\t'+str(job)+'\t'+identifier+'\t'+str(record)+'\t'+rec_oletype+'\t'+rec_name+'\t'+rec_category+'\t'+rec_inputpins+'\t'+rec_stagetype+'\t'+str(subrecord)+'\t'+subrec_name+'\t'+subrec_value+'\n')  # Header record
           wrote_record=True
           subrec_name=''; subrec_value=''
       s_cnt=0
 
     # At the end of each job record:
     if not wrote_record:
-      f.write(project+'\t'+str(job)+'\t'+identifier+'\t'+str(record)+'\t'+rec_oletype+'\t'+rec_name+'\t'+rec_category+'\t'+str(subrecord)+'\t'+subrec_name+'\t'+subrec_value+'\n')  # Header record
+      f.write(project+'\t'+str(job)+'\t'+identifier+'\t'+str(record)+'\t'+rec_oletype+'\t'+rec_name+'\t'+rec_category+'\t'+rec_inputpins+'\t'+rec_stagetype+'\t'+str(subrecord)+'\t'+subrec_name+'\t'+subrec_value+'\n')  # Header record
       wrote_record=False
     # Reset vars:
-    identifier=''; rec_oletype=''; rec_name=''; rec_category=''; subrec_name=''; subrec_value=''; is_usersql=False; wrote_record=False
+    identifier=''; rec_oletype=''; rec_name=''; rec_category=''; rec_inputpins=''; rec_stagetype=''; subrec_name=''; subrec_value=''; is_usersql=False; wrote_record=False
     r_cnt=0
-    s+='\n'
 
     # While debugging, use many fewer rows:
-    # if j_cnt==5:
-    #   break
-
-exit(0)
-
-# Write the delimited string to a file.
-print(f'\n{strftime("%Y-%m-%d %H:%M:%S")}  #{getframeinfo(currentframe()).lineno}: Writing parsed text to file.')
-file_name = strftime("%Y%m%d_%H%M%S")+'_'+parsed_file
-with open(file_name, 'wb') as out:
-  out.write(s.encode('utf-8'))
+    if j_cnt==5:
+      break
